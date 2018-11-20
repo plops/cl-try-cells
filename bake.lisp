52d81b996d929c172555b38af60bb6b14bb03e57
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-11-20 23:00:56 +0100

    observers

--- bake.lisp
+++ bake.lisp
@@ -23,10 +23,41 @@
 
 (defmodel cake ()
   ((allingredientsfu :cell nil :accessor allingredientsfu :initform
-		   *all-ingredients-fu*)
+		     *all-ingredients-fu*)
    (alldonesfu :cell nil :accessor alldonesfu :initform *all-dones-fu*)
    (batter :reader batter :initform '(:milk :egg :flour))
    (icing :reader icing :initform '(:sugar))
    (decoration :reader decoration :initform '(:candles))
-   (todos :reader todos :initform '(:knead :bake :decorate))))
+   (todos :reader todos :initform '(:knead :bake :decorate))
+
+   (mixin :initform (c-in nil))
+   (action :accessor action :initform (c-in nil))
+
+   (ingredients :accessor ingredients :initform (c? (funcall
+						     (allingredientsfu self)
+						     (mixin self))))
+   ;; set-union of the last action and all previous actions
+   (dones :accessor dones :initform (c? (funcall (alldonesfu self)
+						 (action self))))
+   ;; constraint satisfied if subset of ingredients
+   (batter-p :accessor batter-p :initform (c? (subsetp (batter self)
+						       (ingredients
+							self))))
+   (alldone-p :accessor alldone-p :initform (c? (not (set-difference
+						      (todos self)
+						      (dones self)))))))
+
+(defobserver batter-p ((self cake))
+  (when new-value
+    (format t "Batter is complete.~%")))
+
+(defobserver alldone-p ((self cake))
+  (when new-value
+    (format t "Cake is done.~%")))
+
+(defobserver ingredients ((self cake))
+  (format t "Ingredients are now ~a." new-value))
+
+(defobserver dones ((self cake))
+  (format t "Completed tasks are now ~a." new-value))
 
